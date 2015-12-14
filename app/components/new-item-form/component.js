b5e66449b2b537e8e3cf61d9058d4521b8ebc941
Matt Brendzel <matt.brendzel@generalassemb.ly>
2015-12-14 15:06:59 -0500

    Changes properties of the 'new-item-form' component to line up with the `ember-data-crud` repo.
    A `form` property binds directly to the form fields; another property, `newItem`, is a Computed Property which derives its value from `form`, and it is `newItem` that gets passed up through `sendAction` instead of `form`. This intermediation allows for additional logic to be inserted between the form fields and the Route's CRUD action.

--- app/components/new-item-form/component.js
+++ app/components/new-item-form/component.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  newItem: {
+  form: {
     name: null,
     category: null,
     effect: null
   },
+  newItem: Ember.computed('form', function(){
+    return {
+      name: this.get('name'),
+      category: this.get('category'),
+      effect: this.get('effect')
+    }
+  }),
   actions: {
     createItem: function(){
       this.sendAction('routeCreateItem', this.get('newItem'));
