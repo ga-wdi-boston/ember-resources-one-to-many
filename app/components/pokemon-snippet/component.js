16042bfb3768aab9c3845263afd8bb90fb4ed732
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-24 07:16:24 -0400

    Set 'pokemon-snippet' up to use content-editable
    Add in setup from previous material.

--- app/components/pokemon-snippet/component.js
+++ app/components/pokemon-snippet/component.js
@@ -5,14 +5,19 @@ export default Ember.Component.extend({
   twoTypes: Ember.computed('pokemon.types', function(){
     return this.get('pokemon.types').length > 1;
   }),
+  isEditable: false,
   actions: {
     updatePokemon: function(){
       console.log('Component Action : updatePokemon');
       this.sendAction('routeUpdatePokemon', this.get('pokemon'));
+      this.set('isEditable', false);
     },
     destroyPokemon: function(){
       console.log('Component Action : destroyPokemon');
       this.sendAction('routeDestroyPokemon', this.get('pokemon'));
     }
+  },
+  doubleClick: function(){
+    this.toggleProperty('isEditable');
   }
 });
