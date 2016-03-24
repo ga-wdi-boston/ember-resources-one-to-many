b156b6fa0c4b7845d19480bcd0e630b535770813
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-24 07:12:25 -0400

    Remove 'host' from adapter, run with --proxy

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -4,5 +4,4 @@ import ActiveModelAdapter from 'active-model-adapter';
 Ember.Inflector.inflector.uncountable('pokemon');
 
 export default ActiveModelAdapter.extend({
-  host: 'http://localhost:3000'
 });
